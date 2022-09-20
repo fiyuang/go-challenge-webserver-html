9360b80ca33eb67102d988cafa17874d2cb95b8a
fiyuang <fitrianggraini96@gmail.com>
2022-09-20 11:52:43 +0700

    get & deliver data to template

--- main.go
+++ main.go
@@ -41,9 +41,99 @@ func renderTemplate(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var template = template.Must(template.New("home").ParseFiles("index.html"))
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		var email = r.FormValue("email")
+		var keyEmail int
+
+		emails := []string{"fitri@mail.com", "ayu@mail.com", "anggraini@mail.com", "fiyuang@mail.com", "fifiyuu@mail.com"}
+		result := getDataPeserta(emails)
+		// fmt.Println(result)
+
+		// check email exist
+		for key, value := range emails {
+			if email == value {
+				keyEmail = key
+			}
+		}
+
+		// deliver data to template
+		for key, value := range result {
+			if keyEmail == key {
+				var data = map[string]string{
+					"email":     email,
+					"nama":      value.Nama,
+					"alamat":    value.Alamat,
+					"pekerjaan": value.Pekerjaan,
+					"alasan":    value.Alasan,
+				}
+				var err = template.Execute(w, data)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			}
+		}
+
+		return
+	}
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func getDataPeserta(emails []string) []Biodata {
+	var dataPeserta = []Biodata{
+		{
+			Nama:      "Fitri",
+			Alamat:    "Jl. Lorem",
+			Pekerjaan: "Backend",
+			Alasan:    "Alasan Fitri",
+		},
+		{
+			Nama:      "Ayu",
+			Alamat:    "Jl. Ipsum",
+			Pekerjaan: "Frontend",
+			Alasan:    "Alasan Ayu",
+		},
+		{
+			Nama:      "Anggraini",
+			Alamat:    "Jl. Dolor",
+			Pekerjaan: "Fullstack",
+			Alasan:    "Alasan Anggraini",
+		},
+		{
+			Nama:      "Fiyuang",
+			Alamat:    "Jl. Sit",
+			Pekerjaan: "DevOps",
+			Alasan:    "Alasan Fiyuang",
+		},
+		{
+			Nama:      "Fifiyuu",
+			Alamat:    "Jl. Amet",
+			Pekerjaan: "Mobile Dev",
+			Alasan:    "Alasan Fifiyuu",
+		},
+	}
+
+	response := make([]Biodata, 0)
+	var data Biodata
+
+	for key, value := range emails {
+		data.Email = value
+		data.Nama = dataPeserta[key].Nama
+		data.Alamat = dataPeserta[key].Alamat
+		data.Pekerjaan = dataPeserta[key].Pekerjaan
+		data.Alasan = dataPeserta[key].Alasan
+		response = append(response, data)
+	}
+
+	return response
 }
